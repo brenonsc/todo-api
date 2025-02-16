a7a2803b1a82cdb31dcc0b3a33288214fb028a21
Breno Henrique Nascimento <brenonsc@gmail.com>
2025-02-16 11:27:50 -0300

    docs: add Swagger API documentation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+const cors = require('cors');
 const routes = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];
 
-    if (!token) {
+    if (!authHeader) {
         return res.status(401).json({ error: 'No token provided' });
     }
+
+    const parts = authHeader.split(' ');
+
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        return res.status(401).json({ error: 'Token error' });
+    }
+
+    const token = parts[1];
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
